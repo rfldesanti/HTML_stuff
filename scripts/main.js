d255a8233494f19a5bb75ee82a6c0c9c55a71e81
rfldesanti <rafaelsr2018@gmail.com>
2020-12-23 08:07:41 -0300

    Removed old js sripts on main.js

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'What a troll!';
-
-document.querySelector('img').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
