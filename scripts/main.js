afdb8f87652a3eae579bedb89350da113fd8d8b0
rfldesanti <rafaelsr2018@gmail.com>
2020-12-23 08:40:29 -0300

    Add js scripts that change h1 content when image is also changed (onclick)

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
 let myImage = document.querySelector('img');
+let myTitle = document.querySelector('h1');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
 
     if(mySrc === 'images/troll.jpg') {
         myImage.setAttribute('src','images/sapato.jpg');
+        myTitle.textContent = 'Sapatinho!';
     } else {
         myImage.setAttribute('src','images/troll.jpg');
+        myTitle.textContent = 'What a troll!';
     }
 }
