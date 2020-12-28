be5887f8091e03fdf8c77a2d99af87e43d8b5808
rfldesanti <rafaelsr2018@gmail.com>
2020-12-28 10:06:28 -0300

    Line of element button changed to before script elemente in index.html

--- scripts/main.js
+++ scripts/main.js
@@ -19,16 +19,16 @@ let mySubTitle = document.querySelector('h2');
 function setUserName() {
     let myName = prompt('Please enter your name:');
     localStorage.setItem('name', myName);
-    mySubTitle.textContent = 'Mozilla is cool,' + myName;
+    mySubTitle.textContent = 'Mozilla is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')){
     setUserName();
 } else {
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+    mySubTitle.textContent = 'Mozilla is cool, ' + storedName;
 }
 
-myButton.onclick = function() {
-    setUserName();
+myButton.onclick = function () {
+    setUserName();    
 }
