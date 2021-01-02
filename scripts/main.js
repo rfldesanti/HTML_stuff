b9a3d4a6ca078d3c130de01737b15db3189b8e2c
rfldesanti <rafaelsr2018@gmail.com>
2021-01-02 17:35:05 -0300

    Explanations about some properties of the DOM API

--- scripts/main.js
+++ scripts/main.js
@@ -7,16 +7,27 @@ let myTitle = document.querySelector('h1');
 //Here, the variable "myTitle" receive the value of the
 //'h1' document in the HTML document
 
-//Both of the variables above are receiving their values from
-//a search on the document HTML through the method querySelector
+//Both of the variables above receive their values from
+//a search on the document HTML through the method querySelector,
 //which is a method of the DOCUMENT interface - a interface of the
-//wWeb API DOM, that is a API that represents a structure of a document
+//web API DOM, that is a API that represents a structure of a document
 //in memory
-//The querySelector method returns the first element that
+//The querySelector method returns the FIRST ELEMENT that
 //matches the specified selector
 
 myImage.onclick = function() {
+//In this line, the myImage variable, which had it value assigned as
+//an 'img' element from the HTML document, uses the ONCLICK propertie,
+//assigning it to a function. The onclick is an event, that is fired
+//when the element receive a mouse click.
+//Is this case, every instruction inside the block's function
+//will occur when the img element is clicked
     let mySrc = myImage.getAttribute('src');
+    //Here, a variable called mySrc is created, and is assigned to it
+    //the value returned by the getAttribute method, which returns the
+    //value of the attribute parametrized. In this case, mySrc receive
+    //the value found in the src attribute of the myImage variable(that
+    //refers to the img HTML element)
 
     if(mySrc === 'images/troll.jpg') {
         myImage.setAttribute('src','images/sapato.jpg');
